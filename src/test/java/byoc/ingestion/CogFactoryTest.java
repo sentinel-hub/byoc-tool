b6ce4a1d98ac2c3e27d55fecf95a76239f9d45d1
Tomislav Slijepcevic <tomi.slijepcevic@gmail.com>
2020-03-06 11:14:27 +0100

    test cog process
    test cog process

--- src/test/java/byoc/ingestion/CogFactoryTest.java
+++ src/test/java/byoc/ingestion/CogFactoryTest.java
@@ -1,9 +1,19 @@
 package byoc.ingestion;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import byoc.ingestion.ByocIngestor.BandMap;
 import byoc.tiff.TiffDirectory.SampleFormat;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CogFactoryTest {
 
@@ -12,5 +22,21 @@ class CogFactoryTest {
     assertEquals(2, CogFactory.getPredictor(SampleFormat.INT));
     assertEquals(2, CogFactory.getPredictor(SampleFormat.UINT));
     assertEquals(3, CogFactory.getPredictor(SampleFormat.IEEEFP));
+  }
+
+  @ParameterizedTest
+  @CsvSource({
+          "1,R,output_R.tiff",
+          "2,G,output_G.tiff",
+          "3,B,output_B.tiff",
+          "4,dataMask,output_dataMask.tiff"
+  })
+  void cog(int bandIndex, String bandName, String expectedCogName) throws IOException, URISyntaxException {
+    Path inputFile = Paths.get(getClass().getResource("input.tiff").toURI());
+    Path expectedCog = Paths.get(getClass().getResource(expectedCogName).toURI());
+
+    Path actualCog = new CogFactory().createCog(null, inputFile, new BandMap(bandIndex, bandName));
+
+    assertArrayEquals(Files.readAllBytes(expectedCog), Files.readAllBytes(actualCog));
   }
 }
