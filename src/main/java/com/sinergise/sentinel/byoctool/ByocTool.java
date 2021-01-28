9ca110441784ab6c64381d3be921a0e52446718b
Tomislav Slijepcevic <tomi.slijepcevic@gmail.com>
2021-01-28 16:04:23 +0100

    release 0.3.1

--- src/main/java/com/sinergise/sentinel/byoctool/ByocTool.java
+++ src/main/java/com/sinergise/sentinel/byoctool/ByocTool.java
@@ -34,7 +34,7 @@ import java.time.Duration;
 @Log4j2
 public class ByocTool implements Runnable {
 
-  public static final String VERSION = "0.3.0";
+  public static final String VERSION = "0.3.1";
 
   @ArgGroup(exclusive = false)
   private AuthCredentials authCredentials;
