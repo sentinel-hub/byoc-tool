5284b352b28ea59c62fb60e9f58ae0114ad65979
Marjan Sterk <marjan.sterk@sinergise.com>
2021-03-26 12:36:58 +0100

    release 0.4.1

--- src/main/java/com/sinergise/sentinel/byoctool/ByocTool.java
+++ src/main/java/com/sinergise/sentinel/byoctool/ByocTool.java
@@ -34,7 +34,7 @@ import java.time.Duration;
 @Log4j2
 public class ByocTool implements Runnable {
 
-  public static final String VERSION = "0.4.0";
+  public static final String VERSION = "0.4.1";
 
   @ArgGroup(exclusive = false)
   private AuthCredentials authCredentials;
